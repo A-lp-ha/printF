5e5bd5a1225787dc8462381ea76f6700f3f2bfde
A-lp-ha <njugunamwaura14@gmail.com>
2022-07-13 10:08:04 -0700

    l

--- printfunctions.c
+++ printfunctions.c
@@ -14,4 +14,20 @@ int print_char(va_list arg)
 return (_putchar(va_arg(arg, char)));
 }
 
+/**
+* print_string - prints strings
+* @arg: arguments
+* Return: 1 successfull, -1 unsuccessfull
+*/
 
+int print_string(va_list arg)
+{
+int count;
+char *stg = va_arg(arg, char *);
+
+for (count = 0; stg[count]; count++)
+{
+_putchar(stg[count]);
+}
+return (count);
+}
